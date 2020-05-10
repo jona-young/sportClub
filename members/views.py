da0489c46a1a4a1cd3b7e5fbf93bb5bd9a0608b2
jonayoung <young.jon@icloud.com>
2020-05-10 19:09:36 -0400

    Updated homepage for member app

--- members/views.py
+++ members/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Member Home</h1>')
+    return HttpResponse('<h1>Member Home - Member App Feature</h1>')
